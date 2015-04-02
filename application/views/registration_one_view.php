c39f154046b2c5c60f55ad3de1c73e01bcd58793
koeswirocana <koeswirocana@yahoo.co.id>
2015-04-02 22:28:20 +0700

    [DONE] change regis into one form

--- application/views/registration_one_view.php
+++ application/views/registration_one_view.php
@@ -2,9 +2,9 @@
     <br><h5>Registration</h5>
     <div class="card-panel-custom-reg z-depth-1">
         <div class="row">
-            <form class="col s6">
+            <form class="col s12">
 				<div class="row">
-                    <form action="#">
+                    <!-- <form action="#">
                         <div class="col s10 offset-s1 input-field file-field">
                             <input class="file-path validate" type="text"/>
                             <div class="btn green">
@@ -12,43 +12,60 @@
                                 <input type="file" />
                             </div>
                         </div>
-                    </form>
+                    </form> -->
+                    <div class="input-field col s5 offset-s1">
+                        <!-- <i class="mdi-action-account-circle prefix"></i> -->
+                        <input id="pic" type="text" class="validate">
+                        <label for="pic">Photo URL</label>
+                    </div>
 
-                    <div class="input-field col s10 offset-s1">
+                    <div class="col s4 offset-s1">
+                        <!-- <i class="mdi-action-account-circle prefix"></i> -->
+                        <select id="gender" type="text" class="validate">
+                            <option value="" disabled selected>Choose your gender</option>
+                            <option value="M">Male</option>
+                            <option value="F">Female</option>
+                        </select>
+                        <label for="gender">Gender</label>
+                    </div>
+
+                    <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="name" type="text" class="validate">
                         <label for="name">Name</label>
                     </div>
 
-                    <div class="input-field col s10 offset-s1">
+                    <div class="col s4 offset-s1">
+                        <select id="faculty" type="text" class="validate">
+                            <option value="" disabled selected>Pick your faculty</option>
+                            <option value="1">Faculty of X</option>
+                            <option value="2">Faculty of Y</option>
+                            <option value="n">Faculty of Z</option>
+                        </select>
+                        <label for="faculty">Faculty</label>
+                    </div>
+
+                    <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="email" type="text" class="validate">
                         <label for="email">Email</label>
                     </div>
 
-                    <div class="input-field col s10 offset-s1">
+
+                    <div class="col s4 offset-s1">
+                        <select id="status" type="text" class="validate">
+                            <option value="" disabled selected>Select your status</option>
+                            <option value="1">Student</option>
+                            <option value="2">Lecturer</option>
+                            <option value="3">Staff</option>
+                        </select>
+                        <label for="status">Status</label>
+                    </div>
+
+                    <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="username" type="text" class="validate">
                         <label for="username">Username</label>
-                    </div>
-
-                    <div class="input-field col s10 offset-s1">
-                        <!-- <i class="mdi-action-account-circle prefix"></i> -->
-                        <input id="password" type="password" class="validate">
-                        <label for="password">Password</label>
-                    </div>
-                </div>
-            </form>
-            <form class="col s6">
-                <div class="row">
-                    <div class="col s5 offset-s1">
-                        <!-- <i class="mdi-action-account-circle prefix"></i> -->
-						<select id="gender" type="text" class="validate">
-							<option value="" disabled selected>Choose your gender</option>
-							<option value="M">Male</option>
-							<option value="F">Female</option>
-						</select>
-                        <label for="gender">Gender</label>
                     </div>
 
                     <div class="col s4 offset-s1">
@@ -57,27 +74,13 @@
                         <label for="birth">Birthday</label>
                     </div>
 
-                    <div class="col s10 offset-s1">
-						<select id="status" type="text" class="validate">
-							<option value="" disabled selected>Select your status</option>
-							<option value="1">Student</option>
-							<option value="2">Lecturer</option>
-							<option value="3">Staff</option>
-						</select>
-                        <label for="status">Status</label>
+                    <div class="input-field col s5 offset-s1">
+                        <!-- <i class="mdi-action-account-circle prefix"></i> -->
+                        <input id="password" type="password" class="validate">
+                        <label for="password">Password</label>
                     </div>
 
-                    <div class="col s10 offset-s1">
-						<select id="faculty" type="text" class="validate">
-							<option value="" disabled selected>Pick your faculty</option>
-							<option value="1">Faculty of X</option>
-							<option value="2">Faculty of Y</option>
-							<option value="n">Faculty of Z</option>
-						</select>
-                        <label for="faculty">Faculty</label>
-                    </div>
-
-                    <div class="col s12 offset-s1">
+                    <div class="col s1 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <!-- <button id="regbtn" class="btn waves-effect waves-light green right-align z-depth-1" type="submit" name="action" href="<?php echo base_url('index.php/registration/step_two') ?>">NEXT</button> -->
                         <a id="next" class="btn waves-effect waves-light green right-align z-depth-1" href="<?php echo base_url('index.php/registration/step_two') ?>" role="button">NEXT</a>
--- application/views/registration_two_view.php
+++ application/views/registration_two_view.php
@@ -2,49 +2,45 @@
     <br><h5>Registration</h5>
     <div class="card-panel-custom-reg z-depth-1">
         <div class="row">
-            <form class="col s6">
+            <form class="col s12">
 				<div class="row">
-                    <div class="input-field col s10 offset-s1">
+                    <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="facebook" type="text" class="validate">
                         <label for="facebook">Facebook Name</label>
                     </div>
 
-                    <div class="input-field col s10 offset-s1">
+                    <div class="input-field col s4 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="twitter" type="text" class="validate">
                         <label for="twitter">Twitter ID</label>
                     </div>
 
-                    <div class="input-field col s10 offset-s1">
+                    <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="hp" type="text" class="validate">
                         <label for="hp">Cellphone Number</label>
                     </div>
 
-                    <div class="input-field col s10 offset-s1">
+                    <div class="input-field col s4 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="bbm" type="text" class="validate">
                         <label for="bbm">BBM Pin</label>
                     </div>
-                </div>
-            </form>
 
-            <form class="col s6">
-                <div class="row">
-                    <div class="input-field col s10 offset-s1">
+                    <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="line" type="text" class="validate">
                         <label for="line">Line ID</label>
                     </div>
 
-                    <div class="input-field col s10 offset-s1">
+                    <div class="input-field col s4 offset-s1">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <input id="whatsapp" type="text" class="validate">
                         <label for="whatsapp">Whatsapp Number</label>
                     </div>
 
-                    <div class="col s12 offset-s1">
+                    <div class="col s1 offset-s7">
                         <!-- <i class="mdi-action-account-circle prefix"></i> -->
                         <button id="regbtn" class="btn waves-effect waves-light green right-align z-depth-1" type="submit" name="action">REGISTER</button>
                     </div>
