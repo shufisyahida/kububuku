bdde22d025429542a178d620ed7b60886e76bc39
Shufi Syahida <semangkaforever@gmail.com>
2015-04-04 04:01:08 +0700

    Card-book di profile

--- application/views/profil_view.php
+++ application/views/profil_view.php
@@ -56,101 +56,102 @@
 				</div>
 			</div>
 		</div>
-
-	<div class="col s12 m6 l9">
-<!--borrowed-->
-			
-
-<!--available-->
-
-<?php 
-if(!empty($koleksiAvailable[0]))
-{
-	echo '<div class="row custom-margin-bottom">
-				<h4>Available</h4>
-				<div class="row valign-wrapper">';
-			        
-					foreach ($koleksiAvailable as $key => $value)
-            		{
-            			echo'
-				        <div class="col s12 m3 l3">
-							<div class="card small">
-								<div class="card-image">
-									<a href="'.base_url('index.php/book/book_info/'.$value->isbn).'"><img src="'.$value->sampul.'" alt="book-cover" class="responsive-img"></a>
-								</div>
-								<div class="card-content">
-									<h6 class="truncate">'.$value->pengarang.'</h6>
-									<p class="divider"></p>
-									<h5 class="truncate">'.$value->judul.'</h5>
-								</div>
-							</div>
-				        </div>';
-				    }
-			      
-			echo '</div>
+		<div class="col s12 m6 l9">
+			<div class="row">
+				<h5>My Collection</h5>
+				<h6>Available</h6>	
 			</div>
-			<div class="row custom-margin-top">
-				<div class="col"><a class="waves-effect waves-green btn-flat">More...</a></div>
-			</div>';
-}
-else
-{
-		echo '<div class="row custom-margin-bottom">
-				<h4>Available</h4>
-				<div class="row valign-wrapper">
-				<div class="col s12 m3 l3">
-					<p>No Collection Available</p>
-				</div>
-				</div>
-			</div>';
-
-}
-?>
-
-
-<!--new-->
-<?php 
-if(!empty($koleksiBorrowed))
-{
-	echo '	<div class="row custom-margin-bottom">
-				<h4>Borrowed</h4>
-				<div class="row valign-wrapper">';
-					foreach ($koleksiBorrowed as $key => $value)
-            		{
-            			echo'
-				        <div class="col s12 m3 l3">
-							<div class="card small">
-								<div class="card-image">
-									<a href="'.base_url('index.php/book/book_info/'.$value->isbn).'"><img src="'.$value->sampul.'" alt="book-cover" class="responsive-img"></a>
-								</div>
-								<div class="card-content">
-									<h6 class="truncate">'.$value->pengarang.'</h6>
-									<p class="divider"></p>
-									<h5 class="truncate">'.$value->judul.'</h5>
-								</div>
-							</div>
-				        </div>';
-				    }
-			    echo '</div>
+			<div class="row">
+				<?php
+					if(!empty($koleksiAvailable[0]))
+					{
+						foreach ($koleksiAvailable as $key => $value)
+						{
+							echo '<div class="col s12 m12 l6">
+							        <div class="card card-book">
+							          	<div class="row row-custom-a">
+								            <div class="col s4 m4 l4">
+								              	<a href="'.base_url('index.php/book/book_info/'.$value->isbn).'"><img src="'.$value->sampul.'" alt="book-cover" class="responsive-img"></a>
+								            </div>
+								            <div class="col s8 m8 l8">
+								            	<span class="card-book-title black-text">'.$value->judul.'</span><br>
+								            	<span>'.$value->pengarang.'</span><br>
+								            	<span class="tag-property white-text green">'.$value->genre.'</span><br><br>
+								            	<div class="row row-custom-a">
+								            	    <a class="waves-effect waves-green black-text btn-flat">Borrow</a>
+								            	</div>
+								            </div>
+							          	</div>
+							        </div>
+							    </div>';
+						}
+					}
+					else
+					{
+						echo '<div class="col s12 m12 l12">
+									<p>No Collection Available</p>
+								</div>';
+					}
+				?>
 			</div>
-			<div class="row custom-margin-top">
-				<div class="col"><a class="waves-effect waves-green btn-flat">More...</a></div>
-			</div>';
-}
-else
-{
-		echo '<div class="row custom-margin-bottom">
-				<h4>Borrowed</h4>
-				<div class="row valign-wrapper">
-				<div class="col s12 m3 l3">
-					<p>No Collection Borrowed</p>
-				</div>
-				</div>
-			</div>';
-
-}
-?>
-<!--available-->
+			<div class="row">
+				<h6>Borrowed</h6>
+			</div>
+			<div class="row">
+				<?php
+					if(!empty($koleksiBorrowed))
+					{
+						foreach($koleksiBorrowed as $key => $value)
+						{
+							echo '<div class="col s12 m12 l6">
+							        <div class="card card-book">
+							          	<div class="row row-custom-a">
+								            <div class="col s4 m4 l4">
+								              	<a href="'.base_url('index.php/book/book_info/'.$value->isbn).'"><img src="'.$value->sampul.'" alt="book-cover" class="responsive-img"></a>
+								            </div>
+								            <div class="col s8 m8 l8">
+								            	<span class="card-book-title black-text">'.$value->judul.'</span><br>
+								            	<span>'.$value->pengarang.'</span><br>
+								            	<span class="tag-property white-text green">'.$value->genre.'</span><br><br>
+								            	<div class="row row-custom-a">
+								            	    <a class="btn-flat disabled">Borrowed</a>
+								            	</div>
+								            </div>
+							          	</div>
+							        </div>
+							    </div>';
+						}
+					} else {
+						echo '<div class="col s12 m3 l3">
+								<p>No Collection Borrowed</p>
+							</div>';
+					}
+				?>
+			</div>
+			<div class="divider"></div>
+			<div class="row">
+				<h5>My Wishlist</h5>
+			</div>
+			<div class="row">
+				<!--<div class="col s12 m12 l6">
+			        <div class="card card-book">
+			          	<div class="row row-custom-a">
+				            <div class="col s4 m4 l4">
+				              	<img class="responsive-img" src="<?php echo base_url('assets/img/cover1.jpg') ?>">
+				            </div>
+				            <div class="col s8 m8 l8">
+				            	<span class="card-book-title black-text">The Lord of the Rings (The Lord of the Rings #1-3)</span><br>
+				            	<span>J.R.R. Tolkien</span><br>
+				            	<span class="tag-property white-text green">Fiction</span><br><br>
+				            	<div class="row row-custom-a">
+				            	    <a class="waves-effect waves-green black-text btn-flat">Inform</a>
+				            	</div>
+				            </div>
+			          	</div>
+			        </div>
+			    </div>-->
+			    Coming soon
+			</div>
 		</div>
 	</div>
 </div>
