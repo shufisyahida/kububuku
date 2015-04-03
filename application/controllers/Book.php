055bc81aa4a2b6efad8d9e0fd183df20add3bc81
mustikapril <mustika.musang@gmail.com>
2015-04-03 23:38:39 +0700

    [DONE] Melihat buku :3 #GILS GILS GUE

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -1,10 +1,11 @@
 <?php
     class Book extends CI_Controller{
-        public function book_info()
+        public function book_info($isbn)
         {
-            $isbn = $this->session->userdata('isbn');
+            //$isbn = $this->session->userdata('isbn');
             $this->load->model('buku');
-            $data['result'] = $this->buku->getBook($isbn);
+            $data['resultBook'] = $this->buku->getBook($isbn);
+            $data['resultOwner']= $this->buku->getOwner($isbn);
         	$this->load->view('head_view');
             $this->load->view('navbar_view');
 
--- application/models/buku.php
+++ application/models/buku.php
@@ -36,10 +36,20 @@ class Buku extends CI_Model
 		$this->db->select('*');
 		$this->db->from('buku');
 		$this->db->where('isbn',$isbn);
-
-		$query=$this->db->get()->result();
-
+		$query=$this->db->get();
 		return $resultBook = $query->result();
+		
+	}
+
+	function getOwner($isbn)
+	{
+
+		 $this->db->select("non_admin.foto");
+  		 $this->db->from('non_admin');
+  		 $this->db->join('koleksi', 'koleksi.username = non_admin.username');
+  		 $this->db->where('koleksi.isbn',$isbn);
+  		 $query = $this->db->get();
+  		 return $resultOwner = $query->result();
 		
 	}
 
