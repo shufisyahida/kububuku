67ad7fdf3def62b54fcdf732659d6ad3a5b5e32e
Shufi Syahida <semangkaforever@gmail.com>
2015-04-03 19:27:50 +0700

    [DONE] - benerin navbar - benerin search user - benerin search book

--- application/views/navbar_view.php
+++ application/views/navbar_view.php
@@ -11,14 +11,7 @@
       <div class="nav-container nav-wrapper">
         <a href="<?php echo base_url('index.php/dashboard/') ?>" class="brand-logo"><img class="img-logo responsive-img" src="<?php echo base_url('assets/img/logo-horizontal.png') ?>"></a>
         <ul class="right hide-on-med-and-down">
-          <li>
-            <form>
-              <div class="input-field">
-                <label for="search"><i class="lime-text text-lighten-5 mdi-action-search"></i></label>
-                <input id="search" type="text" required>
-              </div>
-            </form>
-          </li>
+          <li><a href="#"><i class="lime-text text-lighten-5 mdi-action-search"></i></a></li>
           <li><a href="#"><i class="lime-text text-lighten-5 mdi-social-notifications"></i></a></li>
           <li><a class="custom-a dropdown-button" href="#!" data-activates="dropdown1"><img class="img-icon circle responsive-img" src="<?php echo base_url('assets/img/fallon.jpg') ?>"></a></li>
         </ul>
