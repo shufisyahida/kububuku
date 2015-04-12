a97390bb364aeb69c31ade64d663c7441bb95e87
koeswirocana <koeswirocana@yahoo.co.id>
2015-04-12 10:41:43 +0700

    [NOT COMPLETE] nambahin slider buat give_rank

--- application/views/request_in_view.php
+++ application/views/request_in_view.php
@@ -107,9 +107,12 @@
 							echo '<div id="modal-ranking'.$index.'" class="modal">
 								<div class="modal-content">
 									<h4>Give Rank</h4>
-									<p>Si X has return your book, give some rank.</p>
+									<p class="range-field">
+										<input type="range" name="borrower-rank" id="borrower-rank" min="1" max="5" />
+									</p>
 								</div>
-								<div class="modal-footer">';
+								<div class="modal-footer">
+								<a href="#" class="waves-effect waves-red btn-flat modal-action modal-close">Cancel</a>';
 								echo '<a href="'.base_url()."index.php/request_in/confirmReturn/".$idPinjaman[$index].'" class="waves-effect waves-green btn-flat modal-action">OK</a>
 								</div>
 							</div>';
