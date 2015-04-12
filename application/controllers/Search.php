1503b187dab5b9348730560422b88ed1f07d4ce1
Elka Pratika Ilman <elkapilman@gmail.com>
2015-04-12 20:05:02 +0700

    Edit profil tinggal foto. Search push buatan mumus (udah bisa dipake). Add Book kalo buku belom ada [done].

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -17,36 +17,114 @@
 
         public function cariBuku()
         { 
-             $filters['keyword'] = $this->input->post('keyword', TRUE);
-             $filters['kategori'] = $this->input->post('kategori', TRUE);
-             $data = $this->search_model->searchBuku($filters);
-         
+              $judulPengarang = $this->input->post('keyword');
+             $kategori = $this->input->post('kategori');
+             $genre = $this->input->post('genre');
+           
+             $this->load->model('search_model');
 
-            
+             if($judulPengarang!=null){
+                $keyword=$judulPengarang;
+             }
+             else{
+                $keyword=$genre;
+             }
+             
+
+             if($kategori!=null && $keyword!=null){
+                 $result = $this->search_model->searchBuku($keyword,$kategori);
+                   if($result==false) {
+                       $data['notFound'] = "Sorry, no records found";
+                       $data['resultSearchBuku'] = null;
+                        $data['notMatch'] = null;
+                      
+                    }
+                    else {
+                      $data['resultSearchBuku'] = $result;
+                      $data['notFound'] = null;
+                      $data['notMatch'] = null;
+                    } 
+             }
+             else if($kategori==null)
+             {
+                   $data['notMatch'] = "Choose Category";
+                     $data['resultSearchBuku'] = null;
+                      $data['notFound'] = null;
+             }
+               else if($keyword==null)
+             {
+                   $data['notMatch'] = "Enter keyword";
+                     $data['resultSearchBuku'] = null;
+                      $data['notFound'] = null;
+             }
+
             $this->load->view('head_view');
             $this->load->view('navbar_view');
-            $this->load->view('search_book_view',$data);
-            $this->load->view('foot_view');
+            $this->load->view('search_book_view2',$data);
+            $this->load->view('foot_view'); 
 
 
                         
         }
-
+       
         public function cariPengguna()
         { 
-             $filters['keyword'] = $this->input->post('keyword', TRUE);
-             $filters['kategori'] = $this->input->post('kategori', TRUE);
-             $data = $this->search_model->searchPengguna($filters);
-         
+       
+             $nama = $this->input->post('keyword');
+             $kategori = $this->input->post('kategori');
+             $location = $this->input->post('location');
+             $status = $this->input->post('status');
+             $faculty = $this->input->post('faculty');
+             $this->load->model('search_model');
 
-            
+             if($nama!=null){
+                $keyword=$nama;
+             }
+             else if($location!=null){
+                $keyword=$location;
+             }
+             else if($status!=null){
+                $keyword=$status;
+             }
+             else {
+                $keyword=$faculty;
+             }
+
+             if($kategori!=null && $keyword!=null){
+                 $result = $this->search_model->searchPengguna($keyword,$kategori);
+                   if($result==false) {
+                       $data['notFound'] = "Sorry, no records found";
+                       $data['resultSearchPengguna'] = null;
+                        $data['notMatch'] = null;
+                      
+                    }
+                    else {
+                      $data['resultSearchPengguna'] = $result;
+                      $data['notFound'] = null;
+                      $data['notMatch'] = null;
+                    } 
+             }
+             else if($kategori==null)
+             {
+                   $data['notMatch'] = "Choose Category";
+                     $data['resultSearchPengguna'] = null;
+                      $data['notFound'] = null;
+             }
+               else if($keyword==null)
+             {
+                   $data['notMatch'] = "Enter keyword";
+                     $data['resultSearchPengguna'] = null;
+                      $data['notFound'] = null;
+             }
+
+
+
+
+          
             $this->load->view('head_view');
             $this->load->view('navbar_view');
-            $this->load->view('search_user_view',$data);
-            $this->load->view('foot_view');
-
-
-                        
+            $this->load->view('search_user_view2',$data);
+            $this->load->view('foot_view');          
         }
     }
 ?>
--- application/views/add_book_view.php
+++ application/views/add_book_view.php
@@ -27,49 +27,43 @@
                         <!-- <i class="mdi-action-perm-contact-cal prefix"></i> -->
                         <input id="pengarang" name="pengarang" type="text" class="validate" value="<?php echo $pengarang;?>">
                         <label for="pengarang">Pengarang</label>
-                        <span class="error"><?php echo $pengarangErr;?></span>
                     </div>
 
                     <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-perm-contact-cal prefix"></i> -->
                         <input id="deskripsi" name="deskripsi" type="text" class="validate" value="<?php echo $deskripsi;?>">
                         <label for="deskripsi">Deskripsi</label>
-                        <span class="error"><?php echo $deskripsiErr;?></span>
                     </div>
 
                     <div class="col s4 offset-s1">
                         <select id="genre" name="genre" type="text" class="validate" >
                               <option value="">Choose book genre</option>
-                              <option value="1" <?php if($genre == "1") echo "selected"; ?>>Genre 1</option>
-                              <option value="2" <?php if($genre == "2") echo "selected"; ?>>Genre 2</option>
-                              <option value="3" <?php if($genre == "3") echo "selected"; ?>>Genre 3</option>
-                              <option value="4" <?php if($genre == "4") echo "selected"; ?>>Genre 4</option>
-                              <option value="5" <?php if($genre == "5") echo "selected"; ?>>Genre 5</option>
-                              <option value="6" <?php if($genre == "6") echo "selected"; ?>>Genre 6</option>                              
+                              <option value="1" <?php if($genre == "1") echo "selected"; ?>>Romance</option>
+                              <option value="2" <?php if($genre == "2") echo "selected"; ?>>Science Fiction</option>
+                              <option value="3" <?php if($genre == "3") echo "selected"; ?>>Fantasy</option>
+                              <option value="4" <?php if($genre == "4") echo "selected"; ?>>Fiction</option>
+                              <option value="5" <?php if($genre == "5") echo "selected"; ?>>Education</option>
+                              <option value="6" <?php if($genre == "6") echo "selected"; ?>>Non-Fiction</option>     
                         </select>
                         <label for="genre">Genre</label>
-                        <span class="error"><?php echo $genreErr;?></span>
                     </div>
 
                     <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-perm-contact-cal prefix"></i> -->
                         <input id="penerbit" name="penerbit" type="text" class="validate" value="<?php echo $penerbit;?>">
                         <label for="penerbit">Penerbit</label>
-                        <span class="error"><?php echo $penerbitErr;?></span>
                     </div>
 
                     <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-perm-contact-cal prefix"></i> -->
                         <input id="tahun_terbit" name="tahun_terbit" type="text" class="validate" value="<?php echo $tahun_terbit;?>">
                         <label for="tahun_terbit">Tahun Terbit</label>
-                        <span class="error"><?php echo $tahun_terbitErr;?></span>
                     </div>
 
                     <div class="input-field col s5 offset-s1">
                         <!-- <i class="mdi-action-perm-contact-cal prefix"></i> -->
                         <input id="jumlah_halaman" name="jumlah_halaman" type="text" class="validate" value="<?php echo $jumlah_halaman;?>">
                         <label for="jumlah_halaman">Jumlah Halaman</label>
-                        <span class="error"><?php echo $jumlah_halamanErr;?></span>
                     </div>
 
                     <div class="input-field col s5 offset-s1">
