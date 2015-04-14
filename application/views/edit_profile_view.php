217fc674733603fc4ae1d764cb8470e8bfbbdfd2
koeswirocana <koeswirocana@yahoo.co.id>
2015-04-15 02:41:23 +0700

    revisi bug di edit_profile --- done

--- application/views/edit_profile_view.php
+++ application/views/edit_profile_view.php
@@ -164,8 +164,8 @@
         $('select').material_select();
     });
 
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 100 // Creates a dropdown of 15 years to control year
-    });
+    // $('.datepicker').pickadate({
+    //     selectMonths: true, // Creates a dropdown to control month
+    //     selectYears: 100 // Creates a dropdown of 15 years to control year
+    // });
 </script>
