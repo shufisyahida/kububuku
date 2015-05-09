07f8205e6819e8ede6965a54c2e45f33c34c12a2
falloncandra <armyoflooners@yahoo.co.id>
2015-05-10 00:09:57 +0700

    melihat pesan, menghapus pesan sedang diintegrasikan. blom bisa nampilin modal

---
+++ application/controllers/Message.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	class Message extends CI_Controller
+	{
+		public function __construct()
+        {
+            parent::__construct();
+            // $username = $this->session->userdata('username');
+            // if(!$this->session->userdata(''.$username))
+            // {
+            //     redirect(base_url('index.php/Login'));
+            // }
+
+            $this->load->model('pesan');
+
+            
+        }
+
+		public function index()
+		{
+			
+			$this->load->model('pesan');
+			$pesan = $this->pesan->getPesan();
+
+   	           $data['pesan']=$pesan;
+
+			$this->load->view('head_view');
+			$this->load->view('navbar_view');
+            $this->load->view('admin');
+            $this->load->view('pesan_view',$data);
+            $this->load->view('foot_view');
+		}
+
+		public function read($id)
+		{
+			$data = $this->pesan->read($id);
+
+			$this->load->view('pesan',$data);
+		}
+
+		public function delete($id)
+		{
+			$this->pesan->delete($id);
+			redirect(base_url('index.php/Message'));
+		}
+
+
+	}
+?>
--- application/views/pesan_view.php
+++ application/views/pesan_view.php
@@ -1,40 +1,44 @@
 		<div class="col l9">
 			<ul class="collection z-depth-1">
-				<li class="collection-item">
-					<span class="email-address">email@example.com</span><br>
-					<a class="modal-trigger" href="#modal-remove"><span class="title-t">Title<span></a><br>
-					<span class="email-address grey-text">Wednesday, May 7th 2015, 12:00 PM</span>
-					<a href="#!" class="secondary-content"><i class="mdi-content-reply green-text small"></i></a>
-					<a href="#!" class="secondary-content"><i class="mdi-content-clear red-text small"></i></a>
-					<div id="modal-remove" class="modal">
-						<div class="modal-content">
-							<h4>Remove Collection</h4>
-							<p>Are you sure to remove this book from collection?</p>
+			<?php 
+				foreach ($pesan as $key => $value) 
+				{		
+					$newDate = date("d-m-Y , H:i:s",strtotime($value->waktu));
+					$day = date('l', strtotime($value->waktu));
+
+					echo '<div id="modal-message"'.$value->id.'" class="modal">
+							<div class="modal-content">
+								<p>'.$value->isi.'</p>
+							</div>
+							<div class="modal-footer">
+								<a href="#" class="black-text waves-effect waves-red btn-flat modal-action modal-close">Close</a>
+								<a href="'.base_url().'index.php/Message/delete/'.$value->id.')" class="black-text waves-effect waves-green btn-flat modal-action">Remove</a>
+							</div>
 						</div>
-						<div class="modal-footer">
-							<a href="#" class="black-text waves-effect waves-red btn-flat modal-action modal-close">Cancel</a>
-							<a href="#" class="black-text waves-effect waves-green btn-flat modal-action">remove</a>
+
+					<li class="collection-item">
+						<span class="email-address">'.$value->username.'</span><br>
+						<a class="modal-trigger" href="#modal-message'.$value->id.'"><span class="title-t">Title<span></a><br>
+						<span class="email-address grey-text">'.$day." , ".$newDate.'</span>
+						
+						
+						<div id="modal-remove'.$value->id.'" class="modal">
+							<div class="modal-content">
+								<h4>Remove Collection</h4>
+								<p>Are you sure to remove this book from collection?</p>
+							</div>
+							<div class="modal-footer">
+								<a href="#" class="black-text waves-effect waves-red btn-flat modal-action modal-close">Cancel</a>
+								<a href="'.base_url().'index.php/Message/delete/'.$value->id.')" class="black-text waves-effect waves-green btn-flat modal-action">Remove</a>
+							</div>
 						</div>
-					</div>
-				</li>
-				<li class="collection-item">
-					<span class="email-address">email@example.com</span><br>
-					<a class="modal-trigger" href="#modal-remove"><span class="title-t">Title<span></a><br>
-					<span class="email-address grey-text">Wednesday, May 7th 2015, 12:00 PM</span>
-					<a href="#!" class="secondary-content"><i class="mdi-content-reply green-text small"></i></a>
-					<a href="#!" class="secondary-content"><i class="mdi-content-clear red-text small"></i></a>
-					<div id="modal-remove" class="modal">
-						<div class="modal-content">
-							<h4>Remove Collection</h4>
-							<p>Are you sure to remove this book from collection?</p>
-						</div>
-						<div class="modal-footer">
-							<a href="#" class="black-text waves-effect waves-red btn-flat modal-action modal-close">Cancel</a>
-							<a href="#" class="black-text waves-effect waves-green btn-flat modal-action">remove</a>
-						</div>
-					</div>
-				</li>
-				
+
+						<a href="#!" class="secondary-content"><i class="mdi-content-reply green-text small"></i></a>
+						<a href="#modal-remove'.$value->id.'" class=" modal-trigger secondary-content "><i class="mdi-content-clear red-text small"></i></a>
+
+
+					</li>';
+				} ?>				
 			</ul>
 		</div>
 	</div>
